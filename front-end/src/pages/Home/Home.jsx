ed509182e039177336cfd3781eafa215be538d81
Legend373 <164746195+Legend373@users.noreply.github.com>
2025-04-25 18:57:53 -0300

    finishing with navigation and header components

---
+++ front-end/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+
+const Home = () => {
+    const [category, setCategory] = useState("all")
+    return (
+        <div>
+            <Header />
+            <ExploreMenu category={category} setCategory={setCategory} />
+        </div>
+    )
+}
+
+export default Home
