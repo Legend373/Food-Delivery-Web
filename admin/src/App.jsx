5337d2c6b4f427735d8943a9a4fda1268fe9c5a1
Legend373 <164746195+Legend373@users.noreply.github.com>
2025-05-04 17:52:35 -0300

    setting up the admin front end

---
+++ admin/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/sidebar/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Add from './pages/add/Add'
+import List from './pages/list/List'
+import Order from './pages/orders/Order'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <hr />
+      <div className='app-content'>
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/order" element={<Order />} />
+        </Routes>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ admin/src/pages/add/Add.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Add.css";
+import { assets } from '../../assets/assets';
+const Add = () => {
+    return (
+        <div className='add'>
+            <form className='flex-col'>
+                <div className='add-image-upload flex-col'>
+                    <p>Upload Image</p>
+                    <label htmlFor='image'>
+                        <img src={assets.upload_area} alt='' />
+                    </label>
+                    <input type='file' id='image' hidden required />
+
+                </div>
+
+            </form>
+
+        </div>
+    )
+}
+
+export default Add
